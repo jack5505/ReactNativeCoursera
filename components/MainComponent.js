2b565b18cc69ebf9669022daa96239d480b66ca7
Jack <jahon.080879@gmail.com>
2020-03-16 18:24:37 +0500

    How to modify menu with css style in reactNative

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,9 +2,9 @@ import React,{Component} from 'react'
 import {DISHES} from '../shared/dishes'
 import Menu from './MenuComponent';
 import Home from './HomeComponent'
-import {View,Platform} from 'react-native'
+import {View,Platform, Image, StyleSheet, ScrollView,Text} from 'react-native'
 import DishDetail from "./DishdetailComponent";
-import { createStackNavigator,createDrawerNavigator }  from 'react-navigation'
+import { createStackNavigator,createDrawerNavigator, DrawerItems, SafeAreaView }  from 'react-navigation'
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import {Icon} from 'react-native-elements'
@@ -33,6 +33,8 @@ const MenuNavigator = createStackNavigator({
         }
     }
 })
+
+
 const HomeNavigator = createStackNavigator({
     Home:{screen: Home},
 },{
@@ -86,6 +88,25 @@ const AboutNavigator = createStackNavigator({
                           onPress={()=> navigation.toggleDrawer()}/>
     })
 })
+
+const CustomDrawerContentComponenet = (props) => (
+        <ScrollView>
+            <SafeAreaView style={styles.container}
+                          forceInset={{top:'always', horizontal:'never'}}>
+                    <View style={styles.drawerHeader}>
+                        <View style={{flex:1}}>
+                            <Image source={require('../shared/images/logo.png')}
+                                   style={styles.drawerImage}
+                                    />
+                        </View>
+                         <View style={{flex:2}}>
+                             <Text style={styles.drawerHeaderText}> Ristorante </Text>
+                         </View>
+                    </View>
+                    <DrawerItems {...props}/>
+            </SafeAreaView>
+        </ScrollView>
+);
 
 const MainNavigator = createDrawerNavigator({
         Home:{
@@ -150,7 +171,8 @@ const MainNavigator = createDrawerNavigator({
         }
 
 },{
-    drawerBackgroundColor:'#D1C4E9'
+    drawerBackgroundColor:'#D1C4E9',
+    contentComponent:CustomDrawerContentComponenet
 })
 
 
@@ -167,4 +189,28 @@ class MainComponent extends Component{
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    drawerHeader:{
+        backgroundColor:'#512DA8',
+        height:140,
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1,
+        flexDirection:'row'
+    },
+    drawerHeaderText:{
+        color:'white',
+        fontSize:24,
+        fontWeight:'bold'
+    },
+    drawerImage:{
+        margin:10,
+        width:80,
+        height:60
+    }
+})
 export default MainComponent;
