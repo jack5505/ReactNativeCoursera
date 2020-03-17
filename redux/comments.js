f517063c90d3c6c14ad4039c28db2ebeb9b97e44
Jack <jahon.080879@gmail.com>
2020-03-17 10:03:54 +0500

    create comments reducer

---
+++ redux/comments.js
@@ -0,0 +1,15 @@
+import * as ActionType from '../redux/ActionTypes'
+
+export const comments = (state = {
+    errMess:null,
+    comments:[]
+},action) =>{
+    switch (action.type) {
+        case ActionType.ADD_COMMENTS:
+            return{...state,isLoading: false,errMess: null,comments:action.payload}
+        case ActionType.COMMENTS_FAILED:
+            return{...state,isLoading: false,errMess:action.payload,comments:[]}
+        default:
+            return state;
+    }
+}
