c0d03b8295609bbb57d4d82f377f1d93e2d55932
Jack <jahon.080879@gmail.com>
2020-03-17 10:46:21 +0500

    created one fully actionCreator for dishes

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,7 +1,7 @@
 import * as ActionType from '../redux/ActionTypes'
 import {baseUrl} from "../shared/baseUrl";
 
-export const fetch = () => (dispatch) => {
+export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl+'comments').then(response => {
         if(response.ok){
             return response;
@@ -28,5 +28,42 @@ export const commentsFailed = (error) => ({
     type: ActionType.COMMENTS_FAILED,
     payload:error
 })
+
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading())
+    return fetch(baseUrl+'dishes').then(response => {
+            if(response.ok){
+                return response;
+            }else{
+                var error = new Error('Error' + response.status+': '+response.statusText)
+                throw  error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .then(error => dispatch(dishesFailed(error.message)));
+};
+
+export const dishesLoading = () => ({
+    type:ActionType.DISHES_LOADING,
+})
+
+export const addDishes = (dishes) => ({
+    type:ActionType.ADD_DISHES,
+    payload:dishes
+})
+
+export const dishesFailed = (error) => ({
+    type:ActionType.DISHES_FAILED,
+    payload:error
+})
+
+
+
+
 
 
