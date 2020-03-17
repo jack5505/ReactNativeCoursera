9230492e677d657e2268e04e42305d3352969872
Jack <jahon.080879@gmail.com>
2020-03-17 09:42:08 +0500

    Imported all packages into node then create configure for redux

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -23,7 +23,6 @@ function RenderDish(props) {
                   color='#f50'
                   onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
                   />
-
         </Card>
         )
     }
