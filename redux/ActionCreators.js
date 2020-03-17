38c366241bc99cfdec8aed63f309b93ff2bdb85f
Jack <jahon.080879@gmail.com>
2020-03-17 11:02:04 +0500

    Setting up Redux

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -63,6 +63,67 @@ export const dishesFailed = (error) => ({
 })
 
 
+export const fetchPromotions = () => (dispatch) => {
+    dispatch(promosLoading())
+    return fetch(baseUrl+'promotions').then(response => {
+            if(response.ok){
+                return response;
+            }else{
+                var error = new Error('Error' + response.status+': '+response.statusText)
+                throw  error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .then(error => dispatch(promosFailed(error.message)));
+};
+export const addPromotions = (promotions) => ({
+    type:ActionType.ADD_PROMOS,
+    payload:promotions
+})
+export const promosFailed = (error) => ({
+    type:ActionType.PROMOS_FAILED,
+    payload:error
+})
+export const promosLoading = () => ({
+    type:ActionType.PROMOS_LOADING,
+})
+
+export const fetchLeaders = () => (dispatch) => {
+    dispatch(leadersLoading())
+    return fetch(baseUrl+'leaders').then(response => {
+            if(response.ok){
+                return response;
+            }else{
+                var error = new Error('Error' + response.status+': '+response.statusText)
+                throw  error;
+            }
+        },
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(leaders => dispatch(addLeaders(leaders)))
+        .then(error => dispatch(leadersFailed(error.message)));
+};
+
+export const addLeaders = (leaders) => ({
+    type:ActionType.ADD_LEADERS,
+    payload:leaders
+})
+export const leadersFailed = (error) => ({
+    type:ActionType.LEADERS_FAILED,
+    payload:error
+})
+export const leadersLoading = () => ({
+    type:ActionType.LEADERS_LOADING,
+})
+
 
 
 
