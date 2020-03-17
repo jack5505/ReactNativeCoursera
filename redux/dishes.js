5c3b23b1b1338babb3f21668140e3bbc64cbacde
Jack <jahon.080879@gmail.com>
2020-03-17 09:54:56 +0500

    Implementation of dish reducer example

---
+++ redux/dishes.js
@@ -0,0 +1,18 @@
+import * as ActionType from '../redux/ActionTypes'
+
+export const dishes = (state = {
+        isLoading:true,
+        errMess:null,
+        dishes:[]
+    },action) =>{
+        switch (action.type) {
+                case ActionType.ADD_DISHES:
+                    return{...state,isLoading: false,errMess: null,dishes:action.payload}
+                case ActionType.DISHES_LOADING:
+                    return{...state,isLoading: true,errMess: null,dishes:[]}
+                case ActionType.DISHES_FAILED:
+                    return{...state,isLoading: false,errMess:action.payload,dishes:[]}
+                default:
+                        return state;
+        }
+    }
