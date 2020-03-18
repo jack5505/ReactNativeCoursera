095c0932ba3c7fa1302536ee3ea62883ef009d14
Jack <jahon.080879@gmail.com>
2020-03-18 22:30:55 +0500

    Everything is fixed ))

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react'
 import {FlatList} from 'react-native'
-import  {ListItem} from 'react-native-elements'
-import {DISHES} from "../shared/dishes";
+import  {Tile} from 'react-native-elements'
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
