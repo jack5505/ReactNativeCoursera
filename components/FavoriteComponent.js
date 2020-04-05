e1b6df912ab362f176bfecc5f2871f6fa6cb826f
Jack <jahon.080879@gmail.com>
2020-04-05 08:52:26 +0500

    Animation

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -45,13 +45,11 @@ class Favorites extends Component{
                                 {
                                     text:'OK',
                                     onPress:()=> this.props.deleteFavorite(item.id),
-                                    style:'cancel'
                                 }
-
 
                             ],
                             {cancelable:false}
-                        )
+                        );
                     }
 
 
